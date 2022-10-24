ca6b461c8dd630c647fb852ede936ac38ae07cce
Minhajmurad <minhajmurad421@gmail.com>
2022-10-24 23:40:31 +0600

    sever search by id created

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const app=express();
 const cors =require('cors');
 const port = process.env.PORT || 5000;
 
+
+const universities = require('./Data/data.json');
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Education Electrical Engineering API Running');
 });
+
+app.get('/universities', (req, res) =>{
+    res.send(universities);
+});
+
+app.get('/universities/:id', (req, res) => {
+    
+    const id =parseInt(req.params.id) ;
+    const selecteduniversitiesid = universities.find(uni => uni.id == id);
+    console.log(selecteduniversitiesid);
+    res.send(selecteduniversitiesid);
+});
+
 
 
 
